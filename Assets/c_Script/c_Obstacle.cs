43ad4270fee2eb4f85537616797568df6a615b73
Seo98 <tjtmdvy8271@naver.com>
2025-07-06 14:55:01 +0900

    <fix> 에러 주석처리
    - 에러주석처리 진행
    - S_개인샘플씬 추가

--- Assets/c_Script/c_Obstacle.cs
+++ Assets/c_Script/c_Obstacle.cs
@@ -7,19 +7,22 @@ public class c_Obstacle : MonoBehaviour
     public enum ObstacleType { SEAWEED, ROCK, WHIRLPOOL, BROKENSHIP }
     public ObstacleType c_obType;
 
-    
-    PlayerMovement s_playMove;
+
+    //PlayerMovement s_playMove;
     bool c_isDamage;
     float c_damage;
 
     private void Start()
     {
-        s_playMove = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+        // s_playMove = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+
+        // S. 오브젝트 태그에 따라 자동지정 
+        // 사전에 해당 오브젝트 프리팹화 및 태그지정필요
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.collider.CompareTag("Player"))
+        if (collision.collider.CompareTag("Player"))
         {
 
             switch (c_obType)
@@ -27,7 +30,7 @@ public class c_Obstacle : MonoBehaviour
                 case ObstacleType.SEAWEED:
                     slowObstacle(false, 0);
                     break;
-                case ObstacleType.ROCK:                  
+                case ObstacleType.ROCK:
                     slowObstacle(true, 30f);
                     break;
                 case ObstacleType.WHIRLPOOL:
@@ -42,22 +45,22 @@ public class c_Obstacle : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        s_playMove.h_boatSpeed = 3f;
+        // s_playMove.h_boatSpeed = 3f;
     }
 
     void slowObstacle(bool c_isDamage, float damage)
     {
-       if(c_isDamage) //데미지 입고 느려짐
+        if (c_isDamage) //데미지 입고 느려짐
         {
-            s_playMove.h_boatSpeed *= 0.2f;
-            s_playMove.nowhp -= damage;  //(nowhp = nowhp- damage)
-            s_playMove.hp = s_playMove.nowhp;
-            Debug.Log($"데미지를 입었습니다. 현재 hp : {s_playMove.hp}");
+            // s_playMove.h_boatSpeed *= 0.2f;
+            // s_playMove.nowhp -= damage;  //(nowhp = nowhp- damage)
+            // s_playMove.hp = s_playMove.nowhp;
+            Debug.Log($"데미지를 입었습니다. 현재 hp : ");
 
         }
-       else //데미지 안 입고 느려짐
+        else //데미지 안 입고 느려짐
         {
-            s_playMove.h_boatSpeed *= 0.2f;
+            // s_playMove.h_boatSpeed *= 0.2f;
             Debug.Log("스턴 걸렸습니다.");
         }
     }
