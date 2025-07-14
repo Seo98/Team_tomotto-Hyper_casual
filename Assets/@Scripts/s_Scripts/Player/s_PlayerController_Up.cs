7e2c4626202cdd87aa0d7deb0ca7c5c37f443645
Seo98 <tjtmdvy8271@naver.com>
2025-07-14 18:40:32 +0900

    [Test] 플레이어 위로 이동 추가
    - 시점확인용 테스트
    - Grid1 잔여 오브젝트 제거

---
+++ Assets/@Scripts/s_Scripts/Player/s_PlayerController_Up.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class s_PlayerController_Up : MonoBehaviour
+{
+    [SerializeField]
+    private s_PlayerInfo s_playerInfo; // 플레이어의 정보
+
+    private Rigidbody2D s_rb;
+    private Vector2 s_moveDirection;
+
+    void Awake()
+    {
+        // 겟 컴포넌트
+        s_playerInfo = GetComponent<s_PlayerInfo>();
+        s_rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // 입력 처리
+        HandleInput();
+    }
+
+    void FixedUpdate()
+    {
+        // 움직임 처리
+        HandleMovement();
+    }
+
+    private void HandleInput()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 direction = new Vector2(moveHorizontal, 1); // 위로 이동 포함
+        s_moveDirection = direction.normalized;
+    }
+
+    private void HandleMovement()
+    {
+        s_rb.linearVelocity = s_moveDirection * s_playerInfo.s_moveSpeed;
+    }
+}
