f6f94f786a48f8af5e1a7da035f5800f74c48741
Sooezfor <sangkyu3395@naver.com>
2025-07-16 22:27:54 +0900

    <refact> 대포알 자동공격으로 수정
    그 외 씬 생성, 피버 시 공격 기능 제외 처리.

--- Assets/@Scripts/c_Script/s_PlayerController.cs
+++ Assets/@Scripts/c_Script/s_PlayerController.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 public class s_PlayerController : MonoBehaviour
 {
     [SerializeField]
+
+    c_FeverTimeManage c_fever;
     private s_PlayerInfo s_playerInfo; // 플레이어의 정보
+    public GameObject c_cannonPrefab;
+    public GameObject c_firePosition;
+
+    float c_spawnTime = 2f;
+    float c_timer;
 
     private Rigidbody2D s_rb;
     private Vector2 s_moveDirection;
@@ -13,12 +20,19 @@ public class s_PlayerController : MonoBehaviour
         // 겟 컴포넌트
         s_playerInfo = GetComponent<s_PlayerInfo>();
         s_rb = GetComponent<Rigidbody2D>();
+        c_fever = FindFirstObjectByType<c_FeverTimeManage>();
     }
 
     void Update()
     {
+        c_timer += Time.deltaTime;
         // 입력 처리
         HandleInput();
+        if(c_timer > c_spawnTime && !c_fever.c_isFever) //2초마다 대포알 생성
+        {
+            c_timer = 0; 
+            GameObject c_cannonball = Instantiate(c_cannonPrefab, c_firePosition.transform.position, Quaternion.identity);
+        }        
     }
 
     void FixedUpdate()
