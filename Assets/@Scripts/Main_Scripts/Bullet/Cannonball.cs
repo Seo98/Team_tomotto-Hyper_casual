54b8a35123e47eb18d9a35f73fca20dba382a821
Sooezfor <sangkyu3395@naver.com>
2025-08-09 13:58:32 +0900

    c_refactoring test

--- Assets/@Scripts/Main_Scripts/Bullet/Cannonball.cs
+++ Assets/@Scripts/Main_Scripts/Bullet/Cannonball.cs
@@ -1,21 +1,28 @@
 using System;
+using System.Collections;
 using UnityEngine;
 public class Cannonball : MonoBehaviour
 {
-    
     public float speed = 10f;
-    
+
     Vector3 dir = Vector3.up;
 
     private void Update()
     {
         transform.position += dir * speed * Time.deltaTime;
         //위쪽으로 나간다
-    }       
+    }
 
     //충돌한 게 몬스터이거나 보스면 총알 사라지기
     private void OnCollisionEnter2D(Collision2D other) // 아마 여기서 참조해서 score ++ exp ++... 
     {
-        if (other.transform.CompareTag("Monster") || other.transform.CompareTag("Boss")) Destroy(gameObject);
+        if (other.transform.CompareTag("Monster") || other.transform.CompareTag("Boss")) Destroy(this.gameObject);
+
+        if (other.transform.CompareTag("Monster"))
+        {
+            other.transform.GetComponent<Jellyfish>().particle.SetActive(true);
+            other.transform.GetComponent<Shark>().particle.SetActive(true); //젤리피시 때리면 널 레퍼런스 
+            other.transform.GetComponent<Octopus>().particle.SetActive(true); //동일한 이유로 널레퍼런스 
+        }
     }
 }
